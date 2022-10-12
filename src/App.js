21e35c4c22bb2322d30d716d243cd1403ecd4630
Elena Sviridova <valeryk13@yandex.ru>
2022-10-12 15:01:24 +0300

    AboutUs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import AboutUs from './components/AboutUs';
 import Header from './components/Header'
 import Main from './components/Main';
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <AboutUs />
     </>
   );
 }
---
+++ src/components/AboutUs.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './AboutUs.css';
+import  icon_first from '../images/icon-people.svg';
+import  icon_second from '../images/icon-explorer.svg';
+
+function AboutUs() {
+    return (
+      <div className="about_us">
+          <h1 className='about_us_title'>About Us</h1>
+          <div className='about_us_list'>
+            <article className='about_us_card'>
+                <div className='about_us_frame'>
+                    <img className='about_us_icon' alt='icon' src={icon_first}></img>
+                    <span className='about_us_span'>High level of specialists qualification</span>
+                </div> 
+            </article>
+            <article className='about_us_card'>
+                <div className='about_us_frame'>
+                    <img className='about_us_icon' alt='icon' src={icon_second}></img>
+                    <span className='about_us_span'>Great experience</span>
+                </div>
+            </article>
+            <article className='about_us_card'>
+                <div className='about_us_frame'>
+                    <img className='about_us_icon' alt='icon' src={icon_first}></img>
+                    <span className='about_us_span'>High level of specialists qualification</span>
+                </div>
+            </article>
+          </div>
+      </div> 
+    );
+  }
+  
+  export default AboutUs;
